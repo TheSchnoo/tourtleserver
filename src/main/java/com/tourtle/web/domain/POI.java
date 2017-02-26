b6547967d117ceb3f021d5fe44fdfdc98d04e503
wes <wes.berry1@gmail.com>
2017-02-25 22:53:15 -0800

    Fixing to API contract.

--- src/main/java/com/tourtle/web/domain/POI.java
+++ src/main/java/com/tourtle/web/domain/POI.java
@@ -1,12 +1,14 @@
 package com.tourtle.web.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.tourtle.web.domain.domainviews.Views;
 import lombok.Data;
 
 @Data
 public class POI {
-    @JsonView(Views.ResponseData.class) String beaconId;
+    @JsonView(Views.ResponseData.class)
+    @JsonProperty("uuid") String beaconId;
     @JsonView(Views.ResponseData.class) String name;
     @JsonView(Views.ResponseData.class) Double lat;
     @JsonView(Views.ResponseData.class) Double lon;
