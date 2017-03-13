a4fbb5e045c3770e101709911b7eb656579ce271
wes <wes.berry1@gmail.com>
2017-03-12 23:38:28 -0700

    Testing test fix; disabling C3P0.

--- src/main/java/com/tourtle/TourtleServerApplication.java
+++ src/main/java/com/tourtle/TourtleServerApplication.java
@@ -1,8 +1,9 @@
 package com.tourtle;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
+//import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -22,18 +23,27 @@ public class TourtleServerApplication {
 
 		String[] stringArray = System.getenv("CLEARDB_DATABASE_URL").substring(8).split("@");
 
-		ComboPooledDataSource cpds = new ComboPooledDataSource();
-		cpds.setDriverClass("com.mysql.jdbc.Driver");
-		cpds.setJdbcUrl("jdbc:mysql://" + stringArray[1]);
-		cpds.setUser(stringArray[0].split(":")[0]);
-		cpds.setPassword(stringArray[0].split(":")[1]);
-		cpds.setMinPoolSize(5);
-		cpds.setAcquireIncrement(5);
-		cpds.setMaxPoolSize(20);
-		cpds.setAcquireRetryAttempts(5);
-		cpds.setAcquireRetryDelay(3600);
+		return DataSourceBuilder
+				.create()
+				.driverClassName("com.mysql.jdbc.Driver")
+				.username(stringArray[0].split(":")[0])
+				.password(stringArray[0].split(":")[1])
+				.url("jdbc:mysql://" + stringArray[1])
+				.build();
 
-		return cpds;
+
+//		Daa cpds = new ComboPooledDataSource();
+//		cpds.setDriverClass("com.mysql.jdbc.Driver");
+//		cpds.setJdbcUrl("jdbc:mysql://" + stringArray[1]);
+//		cpds.setUser(stringArray[0].split(":")[0]);
+//		cpds.setPassword(stringArray[0].split(":")[1]);
+//		cpds.setMinPoolSize(5);
+//		cpds.setAcquireIncrement(5);
+//		cpds.setMaxPoolSize(20);
+//		cpds.setAcquireRetryAttempts(5);
+//		cpds.setAcquireRetryDelay(3600);
+//
+//		return cpds;
 	}
 
 }
