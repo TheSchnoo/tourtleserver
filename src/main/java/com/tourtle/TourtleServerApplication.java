ab3c026430b66264ce270f7b8191f5bf6af046c2
wes <wes.berry1@gmail.com>
2017-03-12 23:45:55 -0700

    Trial test CI fix.

--- src/main/java/com/tourtle/TourtleServerApplication.java
+++ src/main/java/com/tourtle/TourtleServerApplication.java
@@ -1,14 +1,15 @@
 package com.tourtle;
 
-//import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @SpringBootApplication
 public class TourtleServerApplication {
@@ -19,31 +20,28 @@ public class TourtleServerApplication {
 
 	@Bean
 	@Primary
-	public DataSource dataSource() throws Exception {
+	public DataSource dataSource() throws DataAccessException, PropertyVetoException {
 
-		String[] stringArray = System.getenv("CLEARDB_DATABASE_URL").substring(8).split("@");
+		String[] stringArray;
+		try {
+			stringArray = System.getenv("CLEARDB_DATABASE_URL").substring(8).split("@");
+		} catch (Exception e) {
+			throw e;
+		}
 
-		return DataSourceBuilder
-				.create()
-				.driverClassName("com.mysql.jdbc.Driver")
-				.username(stringArray[0].split(":")[0])
-				.password(stringArray[0].split(":")[1])
-				.url("jdbc:mysql://" + stringArray[1])
-				.build();
+		System.out.println("XXX");
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setDriverClass("com.mysql.jdbc.Driver");
+		cpds.setJdbcUrl("jdbc:mysql://" + stringArray[1]);
+		cpds.setUser(stringArray[0].split(":")[0]);
+		cpds.setPassword(stringArray[0].split(":")[1]);
+		cpds.setMinPoolSize(5);
+		cpds.setAcquireIncrement(5);
+		cpds.setMaxPoolSize(20);
+		cpds.setAcquireRetryAttempts(5);
+		cpds.setAcquireRetryDelay(3600);
 
-
-//		Daa cpds = new ComboPooledDataSource();
-//		cpds.setDriverClass("com.mysql.jdbc.Driver");
-//		cpds.setJdbcUrl("jdbc:mysql://" + stringArray[1]);
-//		cpds.setUser(stringArray[0].split(":")[0]);
-//		cpds.setPassword(stringArray[0].split(":")[1]);
-//		cpds.setMinPoolSize(5);
-//		cpds.setAcquireIncrement(5);
-//		cpds.setMaxPoolSize(20);
-//		cpds.setAcquireRetryAttempts(5);
-//		cpds.setAcquireRetryDelay(3600);
-//
-//		return cpds;
+		return cpds;
 	}
 
 }
