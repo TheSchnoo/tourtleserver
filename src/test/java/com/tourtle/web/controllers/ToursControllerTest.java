f80d31f1ac9b75fcf737b219732fbb782d98b7d8
wes <wes.berry1@gmail.com>
2017-03-16 11:54:16 -0700

    Adding tests.

--- src/test/java/com/tourtle/web/controllers/ToursControllerTest.java
+++ src/test/java/com/tourtle/web/controllers/ToursControllerTest.java
@@ -1,5 +1,6 @@
 package com.tourtle.web.controllers;
 
+import com.tourtle.web.domain.Tour;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.Stack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
@@ -21,14 +25,11 @@ public class ToursControllerTest {
 
     private String BASE_URL = "https://tourtle-app.herokuapp.com";
 
+    private Stack<String> tourIdsToDelete;
+
     @Before
     public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
+        tourIdsToDelete = new Stack<>();
     }
 
     @Test
@@ -45,18 +46,29 @@ public class ToursControllerTest {
     }
 
     @Test
-    public void createTour() throws Exception {
-
+    public void createTourByIdSuccessfullyCreatesATour() throws Exception {
+        Tour tour = new Tour();
+        tour.setName("Testing Tour");
+        tour.setTourId("99");
+        tour.setPois(Collections.emptyList());
+        putTour(tour);
+        String body = restTemplate.getForObject(BASE_URL + "/tours/99", String.class);
+        assertThat(body).contains("tourId");
+        assertThat(body).contains("name");
+        assertThat(body).contains("beacons");
     }
 
-    @Test
-    public void postTour() throws Exception {
-
+    private void putTour(Tour tour) {
+        restTemplate.put(BASE_URL + "/tours/" + tour.getTourId(), tour, String.class);
+        tourIdsToDelete.push(tour.getTourId());
     }
 
-    @Test
-    public void deleteTour() throws Exception {
-
+    @After
+    public void tearDown() throws Exception {
+        while (!tourIdsToDelete.isEmpty()) {
+            String tourId = tourIdsToDelete.pop();
+            restTemplate.delete(BASE_URL + "/tours/" + tourId);
+        }
     }
 
 }
