153e8d86d4bc56d8e6f15a286782438799152bc2
wes <wes.berry1@gmail.com>
2017-03-16 18:52:27 -0700

    Adding tours endpoint happy path tests.

--- src/test/java/com/tourtle/web/controllers/ToursControllerTest.java
+++ src/test/java/com/tourtle/web/controllers/ToursControllerTest.java
@@ -1,6 +1,9 @@
 package com.tourtle.web.controllers;
 
 import com.tourtle.web.domain.Tour;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +17,6 @@ import java.util.Collections;
 import java.util.Stack;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -42,7 +44,12 @@ public class ToursControllerTest {
 
     @Test
     public void getToursById() throws Exception {
-
+        String body = restTemplate.getForObject(BASE_URL + "/tours/2", String.class);
+        assertThat(body).contains("tourId");
+        assertThat(body).contains("name");
+        assertThat(body).contains("beacons");
+        JSONArray beacons = getBeaconArray(body);
+        assertThat(beacons.length()).isGreaterThan(0);
     }
 
     @Test
@@ -54,13 +61,29 @@ public class ToursControllerTest {
         putTour(tour);
         String body = restTemplate.getForObject(BASE_URL + "/tours/99", String.class);
         assertThat(body).contains("tourId");
+        assertThat(body).contains("99");
         assertThat(body).contains("name");
+        assertThat(body).contains("Testing Tour");
         assertThat(body).contains("beacons");
+        JSONArray beacons = getBeaconArray(body);
+        assertThat(beacons.length()).isEqualTo(0);
+
     }
 
     private void putTour(Tour tour) {
         restTemplate.put(BASE_URL + "/tours/" + tour.getTourId(), tour, String.class);
         tourIdsToDelete.push(tour.getTourId());
+    }
+
+    private JSONArray getBeaconArray(String body) {
+        JSONArray beacons = null;
+        try {
+            JSONObject beaconsObject = new JSONObject(body);
+            beacons = beaconsObject.getJSONArray("beacons");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return beacons;
     }
 
     @After
