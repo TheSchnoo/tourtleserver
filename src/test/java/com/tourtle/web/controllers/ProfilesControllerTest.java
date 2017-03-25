8ac07d4c5ea688cf652ca34253d216369b33421a
wes <wes.berry1@gmail.com>
2017-03-25 16:42:00 -0700

    CORS test

--- src/test/java/com/tourtle/web/controllers/ProfilesControllerTest.java
+++ src/test/java/com/tourtle/web/controllers/ProfilesControllerTest.java
@@ -2,12 +2,8 @@ package com.tourtle.web.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
